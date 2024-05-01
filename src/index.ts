e6eadb3a8313dc68d1876a18abadbf49072b137d
André Torres <andretorresjava@gmail.com>
2024-05-01 14:28:38 -0500

    Backend routes.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import userRoutes from './routes/users.routes';
 import { PORT } from './config';
 
 const app = express();
 
-app.listen(PORT);
-console.log('Server on port', PORT);
+app.use('/', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
