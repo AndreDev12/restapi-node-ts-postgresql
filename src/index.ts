e4895f3a6547e71b2f6b7a10d092bfee18c2dfcf
André Torres <andretorresjava@gmail.com>
2024-05-02 16:00:25 -0500

    Desarrollar CRUD.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { json } from 'express';
 
 import userRoutes from './routes/users.routes';
 import { PORT } from './config';
 
 const app = express();
 
+app.use(json());
 app.use('/', userRoutes);
 
 app.listen(PORT, () => {
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,26 +4,66 @@ import { pool } from '../db';
 
 const router = Router();
 
-router.get('/users', (req, res) => {
-  res.send('Obteniendo usuarios');
+router.get('/users', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users');
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/users/:id', (req, res) => {
+router.get('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rows } = await pool.query('SELECT * FROM users WHERE id=$1', [id]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'user not found' });
+    }
+    res.json(rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post('/users', async (req, res) => {
+  try {
+    const { id, name, email } = req.body;
+    const { rows } = await pool.query(
+      'INSERT INTO users(id, name, email) VALUES($1, $2, $3) RETURNING *',
+      [id, name, email]
+    );
+    res.json(rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
   const { id } = req.params;
-  res.send(`Obteniendo usuario ${id}`);
+  const { rowCount } = await pool.query(
+    'DELETE FROM users WHERE id=$1 RETURNING *',
+    [id]
+  );
+  if (rowCount === 0) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  // res.send('User deleted');
+  res.sendStatus(204);
 });
 
-router.post('/users', (req, res) => {
-  res.send('Creando usuario');
-});
+router.put('/users/:id', async (req, res) => {
+  const { id, name, email } = req.body;
 
-router.delete('/users/:id', (req, res) => {
-  res.send('Eliminando usuario');
-});
+  await pool.query('UPDATE users SET id=$1, name=$2, email=$3 WHERE id=$4', [
+    id,
+    name,
+    email,
+    req.params.id,
+  ]);
 
-router.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  res.send(`Actualizando usuario ${id}`);
+  res.send(`Actualizando usuario ${req.params.id}`);
 });
 
 export default router;
