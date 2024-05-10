08535f1199878c26788dc50e1be025b5f0a30122
André Torres <andretorresjava@gmail.com>
2024-05-10 12:41:05 -0500

    Validar el protocolo ssl con la variable de entorno TS_NODE_DEV

--- src/db.ts
+++ src/db.ts
@@ -6,4 +6,5 @@ export const pool = new pg.Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: Number(process.env.DB_PORT),
+  ssl: process.env.TS_NODE_DEV === 'true',
 });
